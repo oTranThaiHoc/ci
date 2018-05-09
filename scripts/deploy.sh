a08e2f368e47a6843ba8f1d3d7b697f9686467b0
nguyen.van.hung <nguyen.van.hung@vn-macbook-0004.local>
2018-05-09 16:37:05 +0700

    feat: add custom title

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,7 @@ echo ""
 echo ""
 
 CONFIG_FILE=${PWD}/$1
+TITLE=$2
 
 SCHEME=$(defaults read ${CONFIG_FILE} scheme)
 BUNDLEID=$(defaults read ${CONFIG_FILE} bundleid)
@@ -19,6 +20,12 @@ SERVER=$(defaults read ${CONFIG_FILE} server)
 BUILD_DIR=${PWD}/build_${SCHEME}
 DATE=`date '+%Y%m%d.%H%M%S'`
 BINARY_FILE_NAME=${SCHEME}.${DATE}.ipa
+
+# check title, default ${SCHEME}.${DATE}
+if ! [ -n "${TITLE##+([[:space:]])}" ]; then
+    TITLE=${SCHEME}.${DATE}
+    echo "Using default title ${TITLE}"
+fi
 
 ${PWD}/autobuild.sh ${CONFIG_FILE} ${BINARY_FILE_NAME}
 
@@ -31,7 +38,7 @@ if test $? -eq 0
 fi
 
 echo "Uploading..."
-curl -X POST -F uploadfile=@"${BUILD_DIR}/${BINARY_FILE_NAME}" --form bundleid="${BUNDLEID}" --form title="${SCHEME}.${DATE}" --form project="${SCHEME}" ${SERVER}/upload
+curl -X POST -F uploadfile=@"${BUILD_DIR}/${BINARY_FILE_NAME}" --form bundleid="${BUNDLEID}" --form title="${TITLE}" --form project="${SCHEME}" ${SERVER}/upload
 
 if test $? -eq 0; then
     echo ""
